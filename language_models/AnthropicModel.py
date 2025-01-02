5d84802f787113d42f9501bb19a77434107097e5
musahni_expedia <musahni@expediagroup.com>
2025-01-02 20:36:47 +0530

    removed language models folder

--- language_models/AnthropicModel.py
+++
@@ -1,4 +0,0 @@
-from enum import Enum
-
-class AnthropicModel(Enum):
-    SONNET_3_5 = "claude-3-5-sonnet-20241022"
--- language_models/LanguageModelAPI.py
+++
@@ -1,9 +0,0 @@
-
-
-class LanguageModelAPI:
-
-    def __init__(self, api_key: str):
-        self.api_key: str = api_key
-
-    def process_pdf(self, file_path: str, prompt: str) -> dict:
-        pass
